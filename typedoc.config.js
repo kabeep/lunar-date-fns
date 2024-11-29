a6a0a0008b3f7bb69c58a030a363e04bfa42d472
kabeep <zzx2067@outlook.com>
2024-11-29 23:04:08 +0800

    chore: add typedoc configuration file

---
+++ typedoc.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    $schema: 'https://typedoc.org/schema.json',
+    entryPoints: ['./src/index.ts'],
+    sort: ['source-order'],
+    projectDocuments: ['./README.*.md', 'LICENSE'],
+    navigationLinks: {
+        Docs: 'https://kabeep.github.io/lunar-date-fns/',
+        GitHub: 'https://github.com/kabeep/lunar-date-fns',
+        NPM: 'https://www.npmjs.com/package/@kabeep/lunar-date-fns',
+    },
+    plugin: [
+        'typedoc-material-theme',
+        'typedoc-plugin-mdn-links',
+        'typedoc-plugin-missing-exports',
+        'typedoc-plugin-include-example',
+        'typedoc-plugin-inline-sources',
+        'typedoc-plugin-replace-text',
+        'typedoc-plugin-rename-documents',
+        'typedoc-plugin-version-header',
+    ],
+    themeColor: '#a2cf6e',
+    renameDocuments: {
+        './README.zh-CN.md': '简体中文',
+    },
+    replaceText: {
+        inCodeCommentText: false,
+        inCodeCommentTags: false,
+        inMarkdown: true,
+        replacements: [
+            {
+                pattern: 'README.md',
+                replace: function () {
+                    return '../index.html';
+                },
+            },
+            {
+                pattern: 'README.zh-CN.md',
+                replace: function () {
+                    return 'documents/____.html';
+                },
+            },
+        ],
+    },
+};
